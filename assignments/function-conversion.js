2fe56b3c5f5ec88134a82ad3c0137eb6fc54f8fd
Daniel Jones <daniel.jones2000@gmail.com>
2019-07-22 15:17:31 -0700

    function arrow finished

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -5,9 +5,8 @@
 // };
 // myFunction();
 
-myFunction => {
-    console.log("Function was invoked!");
-}
+let myFunction = () => console.log("Function was invoked!");
+myFunction();
 
 
 // let anotherFunction = function (param) {
@@ -15,16 +14,35 @@ myFunction => {
 // };
 // anotherFunction("Example");
 
+let anotherFunction = (param) => {return param};
+
+anotherFunction("Example");
+
+console.log(anotherFunction("Example"))
+
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
+
+let add = (param1, param2) => { 
+    return param1 + param2;
+};
+add(1,2);
+
+console.log(add(1,2));
 
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
 
+let subtract = (param1, param2) => {
+  return param1 - param2;
+};
+subtract(1,2);
+
+console.log(subtract(1,2));
 
 // Stretch
 
@@ -33,3 +51,9 @@ myFunction => {
 //   return num * 3;
 // });
 // console.log(triple);
+
+exampleArray = [1,2,3,4];
+const triple = exampleArray.map( (num) => {
+  return num * 3;
+});
+console.log(triple);
